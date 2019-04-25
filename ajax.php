5328f060e1163ecf1e9e984cefb26bf6e648e733
kkxley <kkxley@mail.ru>
2019-04-26 00:07:04 +1100

    создан файл-обработчик ajax

---
+++ ajax.php
@@ -0,0 +1,74 @@
+<?php
+    require_once './db.php';
+
+    global $connect_db;
+
+
+    switch ($_POST['function']){
+        case "sendCommit":
+                sendCommit();
+            break;
+        case "getNewsCommit":
+                getNewsCommit();
+            break;
+
+
+    }
+
+
+    function sendCommit(){
+        session_start();
+        if (isset($_POST['text'])&&isset($_POST['chat_key'])&&isset($_SESSION[$_POST['chat_key']])){
+            global $connect_db;
+
+            $connect_db->setRow(
+                "commits",
+                array("userid", "chat_key", "text_commit", "time"),
+                array(
+                    array(
+                        "userid"=>$_SESSION[$_POST['chat_key']],
+                        "chat_key"=>$_POST['chat_key'],
+                        "text_commit"=>$_POST['text'],
+                        "time"=>time()
+                    )
+                ));
+        }
+        die();
+    }
+
+    function getLastCommit(){
+
+
+    }
+
+    function getNewsCommit(){
+        if (isset($_POST['chat_key'])&&isset($_POST['lastid'])){
+            global $connect_db;
+            session_start();
+            $dt=$connect_db->getRows('commits',
+                array('id',"userid",  "text_commit", "time"),
+                " WHERE chat_key='".$_POST['chat_key']."' AND id>".$_POST['lastid']." AND NOT userid='".$_SESSION[$_POST['chat_key']]."'");
+            foreach ($dt as &$row) {
+                $row['time']=date ( 'H:i d.m.Y' ,$row['time'] );
+            }
+
+            echo json_encode($dt);
+
+        }
+        die();
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+ ?>
