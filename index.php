13cd600816a4e9e111a3a4ddd64003cec1e1792d
kkxley <kkxley@mail.ru>
2019-04-25 21:54:00 +1100

    страница инициальзации чата

--- index.php
+++ index.php
@@ -1,10 +1,18 @@
-<!DOCTYPE html>
-<html lang="ru" dir="ltr">
-    <head>
-        <meta charset="utf-8">
-        <title>Чат</title>
-    </head>
-    <body>
-
-    </body>
-</html>
+<?php
+    require_once "./head.php";
+?>
+    <div class="container main-block d-flex justify-content-center align-items-center">
+        <div class="border p-3 rounded">
+            <form action="chat_list.php" method="post">
+              <div class="form-group">
+                <label for="nameUser">Имя пользователя</label>
+                <input type="text" class="form-control" id="nameUser"  name="login" required>
+              </div>
+              <input type="hidden" name="newchat" value="true">
+              <button type="submit" class="btn btn-dark float-right">Начать чат</button>
+            </form>
+        </div>
+    </div>
+<?php
+    require_once "./footer.php";
+?>
